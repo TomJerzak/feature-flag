9b76ca15682a5847598ef69e928afbfa064670e2
Tom Jerzak <tomasz.jerzak@gmail.com>
2019-09-15 00:02:16 +0200

    Add SetFeatureEnabled method to ExpirationFeatureFlag class.

--- src/FeatureFlag/ExpirationFeatureFlag.cs
+++ src/FeatureFlag/ExpirationFeatureFlag.cs
@@ -1,10 +1,13 @@
 using System;
+using Microsoft.Extensions.Configuration;
 
 namespace FeatureFlag
 {
     public abstract class ExpirationFeatureFlag : IFeatureFlag
     {
-        public DateTime ExpirationDate { get; }
+        private const string ArgumentExceptionMessage = "Defined FeatureFlags must have a value of DateTime type.";
+
+        public DateTime ExpirationDate { get; private set; }
 
         private bool _featureEnabled;
 
@@ -12,20 +15,29 @@ namespace FeatureFlag
         {
             get
             {
-                SetFeatureEnabled(ExpirationDate);
+                UpdateFeatureEnabled(ExpirationDate);
                 return _featureEnabled;
             }
         }
 
         protected ExpirationFeatureFlag(DateTime expirationDate)
         {
-            ExpirationDate = expirationDate;
-            SetFeatureEnabled(expirationDate);
+            UpdateFeatureEnabled(expirationDate);
         }
 
-        private void SetFeatureEnabled(DateTime expirationDate)
+        private void UpdateFeatureEnabled(DateTime expirationDate)
         {
+            ExpirationDate = expirationDate;
             _featureEnabled = expirationDate > DateTime.Now;
+        }
+
+        public void SetFeatureEnabled(IConfigurationSection configurationSection)
+        {
+            if (configurationSection.Exists())
+                if (DateTime.TryParse(configurationSection.Value, out var expirationDate))
+                    UpdateFeatureEnabled(expirationDate);
+                else
+                    throw new ArgumentException(ArgumentExceptionMessage);
         }
     }
 }
